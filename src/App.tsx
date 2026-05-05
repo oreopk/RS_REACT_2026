6a8a7b3ab8cce62c7615cc646067f6952b4d4f72
pkoreo <sart6zaV@yandex.ru>
2026-05-05 03:08:35 +0300

    feat: change base name book for search

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ class App extends React.Component {
 
   fetch = () => {
     const query = this.state.searchWords.trim();
-    const search = query || 'america';
+    const search = query || 'Kingdom';
     if (query !== this.state.oldSearchWords) {
       localStorage.setItem('savedSearch', query);
       this.setState({
