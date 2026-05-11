bfdc711a9fd06bbb3717d6e224f580d436ed1df0
oreopk <sart6zaV@yandex.ru>
2026-05-11 12:22:20 +0300

    feat: add new test localstorage

---
+++ src/tests/components/LocalStorage.test.tsx
@@ -0,0 +1,44 @@
+import { describe, it, expect, vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import App from '../../App';
+import { type Book } from '../../types/book';
+
+const mockFetch = () =>
+  vi.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ numFound: 1, docs: [] as Book[] }),
+    })
+  );
+
+describe('localStorage', () => {
+  it('saved search from localStorage', async () => {
+    localStorage.setItem('savedSearch', 'harry');
+    vi.stubGlobal('fetch', mockFetch());
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('harry');
+    });
+
+    localStorage.clear();
+  });
+
+  it('writes to localStoraged', async () => {
+    vi.stubGlobal('fetch', mockFetch());
+
+    render(<App />);
+    const input = screen.getByRole('textbox');
+
+    fireEvent.change(input, { target: { value: 'hobbit' } });
+    fireEvent.click(screen.getByText('Search'));
+
+    await waitFor(() => {
+      expect(localStorage.getItem('savedSearch')).toBe('hobbit');
+    });
+
+    localStorage.clear();
+  });
+});
