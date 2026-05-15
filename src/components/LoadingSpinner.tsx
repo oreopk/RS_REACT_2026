bf6ea85e437434ba368d15473e4140688099ee3c
pkoreo <sart6zaV@yandex.ru>
2026-05-16 01:36:44 +0300

    feat: loading spinner component change to function component

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import './LoadingSpinner.css';
 
-class LoadingSpinner extends React.Component {
-  render() {
-    return (
-      <div role="status" className="spinner-container">
-        <div className="spinner"></div>
-      </div>
-    );
-  }
+function LoadingSpinner() {
+  return (
+    <div role="status" className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
 }
 
 export default LoadingSpinner;
