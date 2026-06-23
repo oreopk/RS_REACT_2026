03abfbe4bf5d5f28a3831daf916edf9db319f7a7
pkoreo <sart6zaV@yandex.ru>
2026-06-23 03:14:51 +0300

    feat: header client component

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,28 @@
-export default function LocaleLayout({
+import '@/index.css';
+import '@/App.css';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import Header from '@/components/Header';
+
+export default async function LocaleLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) notFound();
+
   return (
-    <html>
-      <body>{children}</body>
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>
+          <Header />
+          {children}
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
